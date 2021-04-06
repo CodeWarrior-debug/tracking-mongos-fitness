6c1da5f1e1853b11813700fd5c0b89312b41a625
CODECENTRAL <jpjordan1987@gmail.com>
2021-04-06 17:40:00 -0400

    trying to figure out what the middleware required problem is

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+// const router = require('express').Router();
+// const htmlroutes = require('./');
+// const apiroutes = require('./apiroutes');
+
+// router.use("/", htmlroutes);
+// router.use("/api", apiroutes);
+
+// module.exports = {};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,10 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//bring in logger/morgan
+app.use(morgan("dev"));
+
 //fill in the proper values
 mongoose.connect("mongodb://localhost/workout", {  useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false});
 
-// 
+// Establish routes 
+app.use(routes);
+
+// start server
 app.listen(PORT, () => {
     console.log(`http://localhost/${PORT}`);
   });
